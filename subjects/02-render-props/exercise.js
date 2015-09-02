f4660c462efdb3eeb0c9580416151f40b7f45072
Michael Jackson <mjijackson@gmail.com>
2015-09-02 07:47:39 -0700

    Update render-props exercise

--- subjects/02-render-props/exercise.js
+++ subjects/02-render-props/exercise.js
@@ -1,9 +1,8 @@
 ////////////////////////////////////////////////////////////////////////////////
-//
 // Exercise:
 //
 // Write a <Tail/> that only logs the last `n` number of rows in a dataset,
-// with an API that allows the developer to control the the rendering.
+// with an API that allows the developer to control the rendering.
 //
 // Hint: You can use a prop that renders a single item, or you can pass all
 // the items to the render prop handing over all rendering control to the
@@ -13,20 +12,22 @@
 //
 // - make the Tail scroll to the bottom when new rows come in
 // - if you didn't already do it this way, make it declarative with a
-//   <PinnedToBottom/> component.
+//   <PinnedToBottom/> component
 // - now make sure if the user scrolls up, you don't scroll them down
 // - make a <JSONP/> component that fetches data with the jsonp package used in
 //   `lib/githubSearch` that uses a render prop to pass its data back up
-//
 ////////////////////////////////////////////////////////////////////////////////
-import React from 'react';
-import { render } from 'react-dom';
-import { listen } from './lib/log';
+import React from 'react'
+import { render } from 'react-dom'
+import { listen } from './lib/log'
 
-var Tail = React.createClass({
-  render () {
+class Tail extends React.Component {
+
+  render() {
+    let { rows } = this.props
+
     return (
-      <div style={{height: 400, overflow: 'auto', border: '1px solid'}}>
+      <div style={{ height: 400, overflow: 'auto', border: '1px solid' }}>
 
         {/* get this rendering outta here and into App */}
         <ul>
@@ -36,38 +37,38 @@ var Tail = React.createClass({
         </ul>
 
       </div>
-    );
+    )
   }
-});
 
-var App = React.createClass({
+}
 
-  getInitialState() {
-    return {
+class App extends React.Component {
+
+  constructor(props, context) {
+    super(props, context)
+
+    this.state = {
       rows: []
     }
-  },
+  }
 
-  componentDidMount () {
+  componentDidMount() {
     listen((newRows) => {
       this.setState({
         rows: this.state.rows.concat(newRows)
-      });
-    });
-  },
+      })
+    })
+  }
 
-  render () {
+  render() {
     return (
       <div>
         <h1>Heads up Eggman, here comes <code>&lt;Tails&gt;</code>s!</h1>
         <Tail rows={this.state.rows}/>
       </div>
-    );
+    )
   }
 
-});
+}
 
 render(<App/>, document.getElementById('app'))
-
-// don't let us forget to talk about `key` here, kthankx.
-
